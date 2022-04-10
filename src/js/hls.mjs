14657b9295565ce1a7ef7e3f8389d0aec17f6057
codi0 <hello@codi.io>
2022-04-10 09:18:46 +0100

    Allow hls to dynamically import dependencies for ipfs:// url

--- src/js/hls.mjs
+++ src/js/hls.mjs
@@ -15,21 +15,48 @@ export var hls = {
 			opts = { callback: opts };
 		}
 		//set vars
+		var prom = Promise.resolve(null);
 		var hls = new Hls({ debug: opts.debug });
 		var mediaEl = document.createElement(opts.type || 'video');
-		//custom loader?
+		//set loader
 		if(opts.loader) {
+			//custom loader
 			hls.config.loader = opts.loader;
-		}
-		//load source
-		hls.loadSource(path);
-		hls.attachMedia(mediaEl);
-		//listen for manifest parsed
-		hls.on(Hls.Events.MANIFEST_PARSED, function() {
-			//set controls
-			mediaEl.controls = true;
-			//exec callback
-			opts.callback(mediaEl);
+		} else if(path.indexOf('ipfs://') === 0) {
+			//ipfs exists?
+			if(!opts.ipfsNode && !globalThis.Ipfs) {
+				//import ipfs module
+				prom = import('./ipfs.mjs').then(function(module) {
+					return module.ipfs();
+				}).catch(function(error) {
+					console.error(error);
+				});
+			} else {
+				//use existing node
+				prom = opts.ipfsNode || Ipfs.create();
+			}
+			//set ipfs loader
+			prom = prom.then(function(ipfsNode) {
+				//import loader module
+				return import('./ipfs/hlsLoader.mjs').then(function(module) {
+					hls.config.loader = module.ipfsHlsLoader(ipfsNode, opts.debug);
+				}).catch(function(error) {
+					console.error(error);
+				});
+			});
+		}		
+		//wait for promise
+		prom.then(function() {
+			//load source
+			hls.loadSource(path);
+			hls.attachMedia(mediaEl);
+			//listen for manifest parsed
+			hls.on(Hls.Events.MANIFEST_PARSED, function() {
+				//set controls
+				mediaEl.controls = true;
+				//exec callback
+				opts.callback(mediaEl);
+			});
 		});
 		//return
 		return hls;
